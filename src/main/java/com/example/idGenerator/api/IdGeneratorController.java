fe4414d31112a6041f357b5ce7574f35efbb747a
saretoduncan <saretoduncan@gmail.com>
2021-11-12 06:03:48 +0300

    add controller

--- src/main/java/com/example/idGenerator/api/IdGeneratorController.java
+++ src/main/java/com/example/idGenerator/api/IdGeneratorController.java
@@ -1,8 +1,30 @@
 package com.example.idGenerator.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.idGenerator.model.Person;
+import com.example.idGenerator.personService.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class IdGeneratorController {
+ private final PersonService personService;
+    @Autowired
+    public IdGeneratorController(PersonService personService) {
+        this.personService = personService;
+    }
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+
+
 
 }
