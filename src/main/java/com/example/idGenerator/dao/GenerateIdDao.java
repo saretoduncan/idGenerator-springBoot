a3376b1fc72461d44b45e1ad1dba52265b7e9c9e
saretoduncan <saretoduncan@gmail.com>
2021-11-12 05:21:03 +0300

    add person model

---
+++ src/main/java/com/example/idGenerator/dao/GenerateIdDao.java
@@ -0,0 +1,5 @@
+package com.example.idGenerator.dao;
+
+public interface GenerateIdDao {
+
+}
---
+++ src/main/java/com/example/idGenerator/model/Person.java
@@ -0,0 +1,22 @@
+package com.example.idGenerator.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
+public class Person {
+    private final String name;
+    private final UUID id;
+    public Person(@JsonProperty("id")UUID id, @JsonProperty("name") String name){
+        this.name= name;
+        this.id= id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+}
